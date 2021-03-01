758cb080fb64839eb90fbf43401f6db1fd50dc22
Nkemjika Obi <nkemjikaobi@Administrators-MacBook-Pro.local>
2021-03-01 13:31:23 +0100

    Changed port and added Procfile

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7002;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
