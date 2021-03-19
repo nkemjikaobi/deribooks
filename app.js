7f0bdfc36b78f4029c8ff23f7ad44dd1287d7cbd
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-6.local>
2021-03-19 11:22:49 +0100

    updates

--- app.js
+++ app.js
@@ -74,10 +74,10 @@ app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
 app.use('/stories', require('./routes/stories'));
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
+// if(process.env.NODE_ENV === 'development'){
+//     app.use(morgan('dev'));
+// }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in mode on port ${PORT}`))
