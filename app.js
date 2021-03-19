c2f90a9b37040e964abab043997d8b2983235e0e
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-6.local>
2021-03-19 11:29:54 +0100

    updates

--- app.js
+++ app.js
@@ -80,4 +80,4 @@ app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
